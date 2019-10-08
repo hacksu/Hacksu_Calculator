19bf8fc916dea1378761897c9316fd771b23ea20
Andrew Hayworth <mhaywort@kent.edu>
2019-10-08 19:51:09 -0400

    changed input style

--- calculator.cpp
+++ calculator.cpp
@@ -4,24 +4,21 @@ int main() {
     char op;
     float num1, num2;
 
-    cout << "Enter operator either + or - or * or /: ";
-    cin >> op;
-
-    cout << "Enter two operands: ";
-    cin >> num1 >> num2;
+    std::cout << "Enter the mathematical statment: Number (+, -, *, /) Number:" << std::endl;
+    std::cin >> num1 >> op >> num2;
 
     switch(op) {
         case '+':
-            std::cout << num1+num2;
+            std::cout << num1 + num2;
             break;
         case '-':
-            std::cout << num1-num2;
+            std::cout << num1 - num2;
             break;
         case '*':
-            std::cout << num1*num2;
+            std::cout << num1 * num2;
             break;
         case '/':
-            std::cout << num1/num2;
+            std::cout << num1 / num2;
             break;
         default:
             // If the operator is other than +, -, * or /, error message is shown
