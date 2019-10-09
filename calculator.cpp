5d06293fe1c075da448cbdf8fdcafb8485a0dd77
Andrew Hayworth <mhaywort@kent.edu>
2019-10-08 20:13:08 -0400

    Added input loop

--- calculator.cpp
+++ calculator.cpp
@@ -1,33 +1,39 @@
 #include <iostream>
 
 int main() {
+    char yor = 'y';
     char op;
     float num1, num2;
 
-    std::cout << "Enter the mathematical statment: Number (+, -, *, /, %) Number:" << std::endl;
-    std::cin >> num1 >> op >> num2;
+    while(yor == 'y' || yor == 'Y') {
+        std::cout << "Enter the mathematical statment: Number (+, -, *, /, %) Number:" << std::endl;
+        std::cin >> num1 >> op >> num2;
+        switch(op) {
+            case '+':
+                std::cout << num1 + num2;
+                break;
+            case '-':
+                std::cout << num1 - num2;
+                break;
+            case '*':
+                std::cout << num1 * num2;
+                break;
+            case '/':
+                std::cout << num1 / num2;
+                break;
+            case '%':
+                std::cout << num1 % num2;
+                break;
+            default:
+                // If the operator is other than +, -, * or /, error message is shown
+                std::cout << "Error! Operator is not correct!" << std::endl;
+                break;
+        }
 
-    switch(op) {
-        case '+':
-            std::cout << num1 + num2;
-            break;
-        case '-':
-            std::cout << num1 - num2;
-            break;
-        case '*':
-            std::cout << num1 * num2;
-            break;
-        case '/':
-            std::cout << num1 / num2;
-            break;
-        case '%':
-            std::cout << num1 % num2;
-            break;
-        default:
-            // If the operator is other than +, -, * or /, error message is shown
-            std::cout << "Error! Operator is not correct!" << std::endl;
-            break;
+        std::cout << "Continue?(Y or N)" << std::endl;
+        std::cin >> yor;
     }
+        
 
     return 0;
 }
