ef52462545647f9f1286b87227f82aa6b09791f4
Andrew Hayworth <mhaywort@kent.edu>
2019-10-08 20:24:27 -0400

    Added Exit Message

--- calculator.cpp
+++ calculator.cpp
@@ -4,7 +4,7 @@ int main() {
     char yor = 'y';
     char op;
     float num1, num2;
-    
+
     while(yor == 'y' || yor == 'Y') {
         std::cout << "Enter the mathematical statment: Number (+, -, *, /, %) Number:" << std::endl;
         std::cin >> num1 >> op >> num2;
@@ -33,37 +33,7 @@ int main() {
         std::cout << "Continue?(Y or N)" << std::endl;
         std::cin >> yor;
     }
-        
-=======
 
-    std::cout << "Enter the mathematical statment: Number (+, -, *, /, %) Number:" << std::endl;
-    std::cin >> num1 >> op >> num2;
-
-    switch(op)
-    {
-
-        case '+': // Adds num1 by num2
-            cout << num1+num2;
-            break;
-        case '-': // Subtracts num1 by num2
-            cout << num1-num2;
-            break;
-        case '*': // Multiplies num1 by num2
-            cout << num1*num2;
-            break;
-        case '/': // Divides num1 by num2
-            cout << num1/num2;
-
-            break;
-        case '%':
-            std::cout << num1 % num2;
-            break;
-        default:
-            // If the operator is other than +, -, * or /, error message is shown
-            std::cout << "Error! Operator is not correct!" << std::endl;
-            break;
-    }
-	return 0;
->>>>>>> da27672ba4e8ab874975d6af178cc6783edfab52
-
+    std::cout << "WOOHOO! The calculations have been calculated!" << std::endl;
+    return 0;
 }
