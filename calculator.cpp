a66bcf4714e225def48e8bf3db4e3566b6fbec1b
Avery-lab <54604339+Avery-lab@users.noreply.github.com>
2019-10-15 00:04:20 -0400

    Changed variables to int for modulus operator

--- calculator.cpp
+++ calculator.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    char op;
-    float num1, num2;
+	char op;
+	float num1, num2;
 
 
-    std::cout << "Enter the mathematical statment: Number (+, -, *, /, %) Number:" << std::endl;
-    std::cin >> num1 >> op >> num2;
+	std::cout << "Enter the mathematical statment: Number (+, -, *, /, %) Number:" << std::endl;
+	std::cout << "Integer operands are required for modulus." << std::endl;
+	std::cin >> num1 >> op >> num2;
 
-    switch(op)
-    {
+	switch (op)
+	{
 
-        case '+': // Adds num1 by num2
-            cout << num1+num2;
-            break;
-        case '-': // Subtracts num1 by num2
-            cout << num1-num2;
-            break;
-        case '*': // Multiplies num1 by num2
-            cout << num1*num2;
-            break;
-        case '/': // Divides num1 by num2
-            cout << num1/num2;
+	case '+': // Adds num1 by num2
+		cout << num1 + num2;
+		break;
+	case '-': // Subtracts num1 by num2
+		cout << num1 - num2;
+		break;
+	case '*': // Multiplies num1 by num2
+		cout << num1 * num2;
+		break;
+	case '/': // Divides num1 by num2
+		cout << num1 / num2;
 
-            break;
-        case '%':
-            std::cout << num1 % num2;
-            break;
-        default:
-            // If the operator is other than +, -, * or /, error message is shown
-            std::cout << "Error! Operator is not correct!" << std::endl;
-            break;
-    }
+		break;
+	case '%':
+		std::cout << static_cast<int>(num1) % static_cast<int>(num2);
+		break;
+	default:
+		// If the operator is other than +, -, * or /, error message is shown
+		std::cout << "Error! Operator is not correct!" << std::endl;
+		break;
+	}
 
 	return 0;
 
