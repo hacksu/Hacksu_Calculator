945921cd8f841c4ef51bef5cabf9c38d3bc39f33
Mahesh Singh Tomar <m9993057178@gmail.com>
2020-10-21 19:18:07 +0530

    mahesh calculator.cpp
    there was no input taking so updated that none of the output values was present and declared corrected it too also in starting it was written usind in place of using so corrected it too

--- calculator.cpp
+++ calculator.cpp
@@ -1,34 +1,35 @@
 // Basic calculator that adds, subtracts, multiplies, and divides
 #include <iostream>
-using std::cin; using std::cout; usind std::endl;
+using std::cin; using std::cout; using std::endl;
 
 int main() {
     char yor = 'y';
     char op;
     float num1, num2;
+    cin>>num1>>op>>num2;
 
 
     switch(op)
     {
 
         case '+': // Adds num1 by num2
-            cout << sub;
+            cout << num1+num2<<"\n";
             break;
         case '-': // Subtracts num1 by num2
-            cout << sub;
+            cout << num1-num2<<"\n";
             break;
         case '*': // Multiplies num1 by num2
-            cout << mult;
+            cout << num1*num2<<"\n";
             break;
         case '/': // Divides num1 by num2
 			if (num2 != 0)
-				cout << div;
+				cout << num1/num2<<"\n";
 			else
 				cout << "You cannot divide by 0!";
             break;
         case '%': // Returns remainder of num1 divided by num2
 			if (num2 != 0)
-				cout << num1 % num2;
+				cout << (int)num1%(int)num2<<"\n";
 			else
 				cout << "You cannot divide by 0!";
             break;
